77e023a38e730954eb8709a3a215904c1da4e72c
Johnson koil Raj <jraj@infoblox.com>
2018-06-07 23:48:02 +0530

    Moving to golang-migrate/migrate (#20)
    mattes/migrate is deprecated

---
+++ vendor/github.com/golang-migrate/migrate/cli/build_go-bindata.go
@@ -0,0 +1,7 @@
+// +build go-bindata
+
+package main
+
+import (
+	_ "github.com/golang-migrate/migrate/source/go-bindata"
+)
---
+++ vendor/github.com/golang-migrate/migrate/cli/build_google-cloud-storage.go
@@ -0,0 +1,7 @@
+// +build google-cloud-storage
+
+package main
+
+import (
+	_ "github.com/golang-migrate/migrate/source/google-cloud-storage"
+)

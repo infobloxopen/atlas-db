ac32aa2ab007e0d55fd38fdda297a0e07c388a7c
shravindran <shravindran@infoblox.com>
2018-06-13 19:15:45 +0530

    CRUD for database users

--- pkg/server/postgres/postgres.go
+++ pkg/server/postgres/postgres.go
@@ -10,6 +10,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 
+	"github.com/golang/glog"
 	atlas "github.com/infobloxopen/atlas-db/pkg/apis/db/v1alpha1"
 	"github.com/infobloxopen/atlas-db/pkg/server/plugin"
 )
@@ -22,6 +23,8 @@ const (
 	defaultPort    = 5432
 	dataPath       = "/var/lib/postgresql/data/pgdata"
 	superUserPwEnv = "POSTGRES_PASSWORD"
+	admin_role     = "createrole createdb"
+	read_role      = "nocreaterole nocreatedb"
 )
 
 type PostgresPlugin atlas.PostgresPlugin
@@ -148,25 +151,33 @@ func (p *PostgresPlugin) SyncDatabase(db *atlas.Database, dsn string) error {
 	if err != nil {
 		return err
 	}
+	defer sqldb.Close()
 	// check if it exists; if not create it
 	rows, err := sqldb.Query(`SELECT 1 FROM pg_database WHERE datname=$1`, db.Name)
 	defer rows.Close()
 	if err != nil {
 		return err
 	}
-
 	if rows.Next() {
+		glog.V(4).Info("Database %s exists", db.Name)
+		err = checkUsers(db, sqldb)
+		if err != nil {
+			return err
+		}
 		return nil
 	}
-
+	glog.V(4).Info("creating database ", db.Name)
 	ddl := fmt.Sprintf(`create database "%s"`, db.Name)
 	_, err = sqldb.Exec(ddl)
 	if err != nil {
 		return err
 	}
-	// check if users exist; verify their passwords and roles
-	// check if other users exist; delete if necessary
-
+	for _, user := range db.Spec.Users {
+		err = createuser(user.Name, user.Role, user.Password, sqldb)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
@@ -174,5 +185,94 @@ func (p *PostgresPlugin) DeleteDatabase(db *atlas.Database) error {
 	// connect
 	// delete users
 	// delete database
+	return nil
+}
+
+func checkUsers(db *atlas.Database, sqldb *sql.DB) error {
+	//// check if users exist; verify their passwords and roles
+	dbUserMap := make(map[string]string)
+	var u string
+	//getting all users from the database
+	rows, err := sqldb.Query(`SELECT u.usename FROM pg_catalog.pg_user u`)
+	if err != nil {
+		return err
+	}
+	for rows.Next() {
+		if err := rows.Scan(&u); err != nil {
+			return err
+		}
+		dbUserMap[u] = "delete-me"
+	}
+	glog.V(4).Info("Users exist in database are ", dbUserMap)
+	users := db.Spec.Users
+	glog.V(4).Info("Users from yaml are ", users)
+
+	for _, user := range users {
+		if _, ok := dbUserMap[user.Name]; !ok {
+			err = createuser(user.Name, user.Role, user.Password, sqldb)
+			if err != nil {
+				return err
+			}
+		} else {
+			dbUserMap[user.Name] = "dont-delete"
+			if user.Role == "admin" {
+				//roles now assigned to admin is createdb, createrole
+				_, err = sqldb.Exec(fmt.Sprintf(`alter user "%s" with %s`, user.Name, admin_role))
+				if err != nil {
+					return err
+				}
+			} else if user.Role == "read" {
+				_, err = sqldb.Exec(fmt.Sprintf(`alter user "%s" with %s`, user.Name, read_role))
+				if err != nil {
+					return err
+				}
+			} else {
+				return fmt.Errorf("role for the user %s is not valid", user.Name)
+			}
+			//There is no straight forward ways to compare password so just updating it
+			_, err = sqldb.Exec(fmt.Sprintf(`alter user "%s" with password '%s'`, user.Name, user.Password))
+			if err != nil {
+				return err
+			}
+		}
+	}
+	//TODO: below will delete all the users in the database. need to delete users for particular app.
+	// delete users
+	//for dbUserName, dbUserVal := range dbUserMap {
+	//	//if delete-me delete database user
+	//	if dbUserVal == "delete-me" && dbUserName != "postgres" {
+	//		glog.Info("deleted user ", dbUserName)
+	//		err = deleteUser(dbUserName, sqldb)
+	//		if err != nil {
+	//			return err
+	//		}
+	//	}
+	//}
+	return nil
+}
+
+func createuser(username, userrole, password string, sqldb *sql.DB) error {
+	if userrole == "admin" {
+		_, err := sqldb.Exec(fmt.Sprintf(`create user "%s" with password '%s' %s`, username, password, admin_role))
+		if err != nil {
+			return err
+		}
+	} else if userrole == "read" {
+		_, err := sqldb.Exec(fmt.Sprintf(`create user "%s" with password '%s' %s`, username, password, read_role))
+		if err != nil {
+			return err
+		}
+	} else {
+		return fmt.Errorf("role for the user %s is not valid", username)
+	}
+	return nil
+}
+
+func deleteUser(userName string, sqldb *sql.DB) error {
+	ddl := fmt.Sprintf(`drop owned by "%s"; drop user "%s"`, userName, userName)
+	_, err := sqldb.Exec(ddl)
+	if err != nil {
+		return err
+	}
 	return nil
 }
